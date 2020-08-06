78bbc5180f80cd2c3747231109648b40053bf468
lailalelouch <lsb-95@hotmail.com>
2020-08-06 21:08:06 +0300

    initialize

---
+++ boards/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Board(models.Model):
+	title = models.CharField(max_length=150)
+	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="boards")
+
+
+class Task(models.Model):
+	description = models.TextField()
+	creation_date = models.DateField(auto_now_add=True)
+	is_hidden = models.BooleanField()
+	is_done = models.BooleanField()
